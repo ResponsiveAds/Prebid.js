0a2dce14b17c5101cd844d304a76a23ca6ef41e7
Primoz <pzagor2@gmail.com>
2024-12-04 09:00:32 +0100

    remove rad example update bidder code

--- modules/responsiveAdsBidAdapter.js
+++ modules/responsiveAdsBidAdapter.js
@@ -1,18 +1,8 @@
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
-import  { ortbConverter } from '../libraries/ortbConverter/converter.js'
+import { BANNER } from '../src/mediaTypes.js';
+import { ortbConverter } from '../libraries/ortbConverter/converter.js'
 import {
-  isArray,
-  replaceAuctionPrice,
-  triggerPixel,
   logMessage,
-  deepSetValue,
-  getBidIdParameter,
-  canAccessWindowTop,
-  getSafeframeGeometry,
-  getWindowSelf,
-  getWindowTop,
-  inIframe,
   isSafeFrameWindow,
 } from '../src/utils.js';
 
@@ -22,11 +12,9 @@ const ENDPOINT_URL = 'https://ve60c4xzl9.execute-api.us-east-1.amazonaws.com/def
 
 const converter = ortbConverter({
     context: {
-        // `mediaType` is required for the `setResponseMediaType` processor
         mediaType: BANNER,
-        // `netRevenue` and `ttl` are required properties of bid responses - provide a default for them
-        netRevenue: true,    // or false if your adapter should set bidResponse.netRevenue = false
-        ttl: 30              // default bidResponse.ttl (when not specified in ORTB response.seatbid[].bid[].exp)
+        netRevenue: true,
+        ttl: 30
     }
 });
 
@@ -38,7 +26,6 @@ export const spec = {
     return !!(bid.params && bid.params.placementId);
   },
   buildRequests: function(bidRequests, bidderRequest) {
-    console.log('isSafeFrameWindow', isSafeFrameWindow());
     //we only want to bid if we are not in a safeframe
     if (isSafeFrameWindow()) {
       return;
